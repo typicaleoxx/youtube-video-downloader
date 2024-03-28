c487f04dab4bde67eaada18e9e6d24ebc55a735d
typicaleoxx <lsneha991@gmail.com>
2024-03-28 21:46:56 +0545

    completed

---
+++ downloader.py
@@ -0,0 +1,37 @@
+from pytube import YouTube
+
+class YouTubeDownloader:
+    def __init__(self, url):
+        '''
+        initialise YouTubeDownloader object with provided url
+        '''
+        self.url=url
+        self.youtube_video=YouTube(self.url)
+
+    def video_info(self):
+        '''
+        prints informationn about the YouTube video
+        '''
+        print("Title:", self.youtube_video.title)
+        print("Views:", self.youtube_video.views)
+
+    def download_video(self,path):
+        '''
+        download the video to the specified path
+        '''
+        download_video=self.youtube_video.streams.get_highest_resolution()
+        print('Downloading.')
+        print('Downloading..')
+        print("Downloading...")
+        download_video.download(path)
+        print('Download complete!')
+
+def main():
+    url=input('Paste the YouTube video URL: ')
+    path='C:/Users/DELL/Downloads'
+    downloader=YouTubeDownloader(url)
+    downloader.video_info()
+    downloader.download_video(path)
+
+if __name__ == "__main__":
+    main()
